d54beb18eddbff9fce3993d592e3a3d050c30147
dofuta <taisei.goro56@gmail.com>
2022-10-10 16:59:00 +0900

    Modify

--- src/components/shared/CircleButton.js
+++ src/components/shared/CircleButton.js
@@ -6,11 +6,9 @@ import CircleArrowSVG from 'public/icons/circle_arrow_black.svg'
 export default function CircleButton(props) {
     const className = `${props.className} ${styles.circleButton} ${styles[props.color]}`
     return (
-        <Link href={props.url} disabled={props.isSending}>
-            <a className={className}>
+            <a className={className} href={props.url} disabled={props.isSending} target="_blank" rel="noopener noreferrer">
                 <CircleArrowSVG className={styles.circleSVG} />
                 <span>{props.title}</span>
             </a>
-        </Link>
     )
 }
