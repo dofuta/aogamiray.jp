b8796a6d1a16212bba2286cbb5b91976ae21cecf
dofuta <taisei.goro56@gmail.com>
2023-10-03 14:33:36 +0900

    Remove consoe.log

--- src/components/index/Goods.js
+++ src/components/index/Goods.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from './Goods.module.scss'
 export default function Goods(props) {
-    console.log(props)
     return (
         <div className={styles.container}>
             <div className={styles.goodsList}>
